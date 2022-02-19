eb459aa684f03ab608ae52e63c0f109daa9ad29a
João paulo C <joaopaulo33573@gmail.com>
2022-02-19 18:57:44 -0300

    Trabalhando propriedades

--- react.js/01-github-explorer/src/components/RepositoryItem.jsx
+++ react.js/01-github-explorer/src/components/RepositoryItem.jsx
@@ -1,11 +1,13 @@
+
 export function RepositoryItem(props){
-    return( <li>
-    {/* Verifica se o argumento não está vazio, se estiver coloca default */}
-    <strong>{props.repository ?? 'Default'} </strong>                 
-    <p>Forms in React</p>
-    <a href="">
-        Acessar repositório
-    </a>
-</li>
-)
+    return( 
+        <li>
+             {/* Verifica se o argumento não está vazio, se estiver coloca default */}
+            <strong>{props.repository?.name ?? 'Default'} </strong>                 
+            <p>{props.repository.description}</p>
+            <a href={props.repository.link}> 
+                Acessar repositório
+            </a>
+         </li>
+    );
 }
--- react.js/01-github-explorer/src/components/RepositoryList.jsx
+++ react.js/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,16 +1,21 @@
 import { RepositoryItem } from "./RepositoryItem";
 
-const repositoryName= 'unform2';
+const repository={
+    name:'unform',
+    description: 'Forms in React',
+    link: 'https://github.com/unform/unform'
+
+}
 export function RepositoryList(){
     return(
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository="uniform2"/>
-                <RepositoryItem/>
-                <RepositoryItem/>
-                <RepositoryItem/>    
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
             </ul>
         </section>
     )
